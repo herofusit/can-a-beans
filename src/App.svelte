04b154a7268642161acaaabd9f70733cda97236b
Hero <hero.fusit@gmail.com>
2020-07-01 15:34:24 -0500

    started settings screen

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,29 @@
 <script>
   import { selectedCan } from "./stores";
   import SeedsList from "./Seeds/SeedsList.svelte";
+  import Settings from "./Settings/Index.svelte";
+  const views = [
+    {
+      name: "seed-list",
+      component: SeedsList
+    },
+    {
+      name: "settings",
+      component: Settings
+    }
+  ];
+
+  let currentView = views[0];
 </script>
 
+<style>
+  :global(.cursor-pointer) {
+    cursor: pointer;
+  }
+</style>
+
 <main>
-  <SeedsList />
+  <i class="material-icons float-right cursor-pointer" on:click={() => (currentView = views[1])}>settings</i>
+  <div class="clearfix" />
+  <svelte:component this={currentView.component} />
 </main>
---
+++ src/Settings/Index.svelte
@@ -0,0 +1,22 @@
+<script>
+  import Input from "../Components/Input.svelte";
+  import Button from "../Components/Button.svelte";
+  let settings = {};
+
+  function saveSettings() {}
+</script>
+
+<div class="card">
+  <div class="card-header card-header-icon card-header-rose">
+    <div class="card-icon">
+      <i class="material-icons">bubble_chart</i>
+    </div>
+    <Button text="save settings" icon="save" classes="float-right" on:click={saveSettings} />
+  </div>
+  <div class="card-body">
+    <!-- <h4 class="card-title"> </h4>
+      <div class="clearfix"></div> -->
+
+    <Input type="number" name="Days in veg" bind:value={settings.defaultDaysInVeg} />
+  </div>
+</div>
